3e436b4faf09a8e80d3f841028d02a1662098a83
ChegeFM254 <chegefm@gmail.com>
2026-07-22 11:32:25 +0300

    Update LoginLockoutServiceTest.java

--- src/test/java/com/mfstechnologies/mymobi/service/LoginLockoutServiceTest.java
+++ src/test/java/com/mfstechnologies/mymobi/service/LoginLockoutServiceTest.java
@@ -8,27 +8,35 @@ class LoginLockoutServiceTest {
 
     @Test
     void noLockoutByDefault() {
-        LoginLockoutService service = new LoginLockoutService();
+        LoginLockoutService service = new LoginLockoutService(0);
         assertThat(service.getLockoutMinutesRemaining("254700000001")).isZero();
     }
 
     @Test
-    void appliedLockoutReportsApproximatelyTenMinutesRemaining() {
-        LoginLockoutService service = new LoginLockoutService();
+    void testingModeDefaultOfZeroSecondsAllowsImmediateRetryAfterLockout() {
+        LoginLockoutService service = new LoginLockoutService(0);
         service.applyLockout("254700000002");
 
-        long remaining = service.getLockoutMinutesRemaining("254700000002");
+        assertThat(service.getLockoutMinutesRemaining("254700000002")).isZero();
+    }
+
+    @Test
+    void uatConfiguredDurationOfTenMinutesActuallyBlocksLogin() {
+        LoginLockoutService service = new LoginLockoutService(600);
+        service.applyLockout("254700000003");
+
+        long remaining = service.getLockoutMinutesRemaining("254700000003");
 
         assertThat(remaining).isBetween(9L, 10L);
     }
 
     @Test
     void lockoutIsPerPhoneNumber() {
-        LoginLockoutService service = new LoginLockoutService();
-        service.applyLockout("254700000003");
+        LoginLockoutService service = new LoginLockoutService(600);
+        service.applyLockout("254700000004");
 
-        assertThat(service.getLockoutMinutesRemaining("254700000003")).isGreaterThan(0);
-        assertThat(service.getLockoutMinutesRemaining("254700000004")).isZero();
+        assertThat(service.getLockoutMinutesRemaining("254700000004")).isGreaterThan(0);
+        assertThat(service.getLockoutMinutesRemaining("254700000005")).isZero();
     }
 
     @Test
