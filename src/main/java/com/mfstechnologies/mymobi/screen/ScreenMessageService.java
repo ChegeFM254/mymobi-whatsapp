e9eb9cfa2813df8182104ded88d278da89f23464
ChegeFM254 <chegefm@gmail.com>
2026-07-23 17:15:19 +0300

    Update ScreenMessageService.java

--- src/main/java/com/mfstechnologies/mymobi/screen/ScreenMessageService.java
+++ src/main/java/com/mfstechnologies/mymobi/screen/ScreenMessageService.java
@@ -61,7 +61,7 @@ public class ScreenMessageService {
     public Mono<Void> sendWelcome(String to) {
         String greeting = userStore.findByPhoneNumber(to)
                 .map(user -> "Hello " + user.getFirstName() + ", Welcome to MyMobi [Java]")
-                .orElse("welcome to MyMobi [Java]");
+                .orElse("Welcome to MyMobi [Java]");
 
         Map<String, Object> payload = Map.of(
                 "messaging_product", "whatsapp",
